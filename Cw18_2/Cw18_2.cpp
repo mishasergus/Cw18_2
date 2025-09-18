d9238a04f5a1d5180bd492e909d895da67a144dc
mishasergus <sergusmisha@gmail.com>
2025-09-18 19:26:19 +0300

    Add project files.

---
+++ Cw18_2/Cw18_2.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <string>
+#include <filesystem>
+#include <fstream>
+#include <Stack>
+
+using namespace std;
+namespace fs = filesystem;
+
+string readFile(string path) {
+	fstream obj1(path, ios::in);
+	string txt;
+	bool next = false;
+	if (obj1.is_open()) {
+		while (getline(obj1, txt)) {
+			txt += "\n";
+		}
+	}
+	obj1.close();
+	return txt;
+}
+void operation(string text, int chastka) {
+	stack<string> st;
+	string temp = "";
+	bool nextIsMinus = false;
+	for (char simv : text) {
+		if (int(simv) >= 48 && int(simv) <= 57 && !nextIsMinus) {
+			temp += simv;
+		}
+		if(int(simv) < 48 || int(simv) > 57){
+			nextIsMinus = false;
+			if (temp != "") {
+				st.push(temp);
+			}
+			temp = "";
+		}
+		if (simv == '-') {
+			nextIsMinus = true;
+		}
+	}
+	while (!st.empty()) {
+		cout << st.top() << " ";
+		st.pop();
+	}
+}
+int main()
+{
+	srand(time(0));
+	string tex = "";
+	tex = readFile("D:/Token/token.txt");
+	cout << tex;
+	
+	
+}
